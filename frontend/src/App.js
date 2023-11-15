cc9405694357f9bb1ca6b826de8122504150e2f0
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-11-14 20:37:17 -0500

    added game container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Nav from './Components/Nav/Nav';
+import { Container } from './Components/Nav/Game Container/Container';
 
 function App() {
 
   return (
     <div>
       <Nav />
+      <main>
+        <Container />
+      </main>
     </div>
   );
 }
---
+++ frontend/src/Components/Nav/Game Container/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Container = () => {
+  return (
+      <div className='border-red-600 border-8 w-[960px] h-[640px] flex mx-auto mt-36'>
+
+
+          
+      </div>
+  )
+}
