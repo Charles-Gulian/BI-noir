a13e54e6159a3fa5cc9048f3d309a6bbe1f54206
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-11-14 20:27:37 -0500

    added tailwind and removed template code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './Components/Nav/Nav';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-
-        ... no changes in this part ...
-
-        <p>The current time is {currentTime}.</p>
-      </header>
+    <div>
+      <Nav />
     </div>
   );
 }
